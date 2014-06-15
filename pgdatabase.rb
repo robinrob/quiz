6f4983040c1cd77174a11737c23e4aad3a3dbc07
msl <msl@mercury.local>
2014-06-15 00:06:00 -0700

    Auto-update

--- pgdatabase.rb
+++ pgdatabase.rb
@@ -1,7 +1,37 @@
-require 'pg'
-require 'active_record'
+class PGDatabase
 
-ActiveRecord::Base.establish_connection(:adapter => "postgresql",
-                                        :username => "postgres",
-                                        :password => "",
-                                        :database => "tagalog")
+  @conn = ActiveRecord::Base.establish_connection(:adapter => "postgresql",
+                                                 :username => "msl",
+                                                 :password => "",
+                                                 :database => "quiz").connection()
+
+  def conn()
+    @conn
+  end
+
+end
+
+
+
+  #def initialize()
+  #  require 'pg'
+  #  require 'active_record'
+  #
+  #
+  #
+  #
+  #  puts conn
+  #
+  #  puts "database: " + conn.current_database
+  #
+  #  puts "rows:"
+  #  result = conn.query('select * from questions')
+  #  puts result
+  #
+  #  #str = 'insert into questions ("id", "question","answer") values (DEFAULT, \'robin\',\'smith\')'
+  #  #puts str
+  #  #
+  #  #result = conn.query(str);
+  #  #
+  #  #puts result.inspect
+  #end
